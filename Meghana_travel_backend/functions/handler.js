680c8e5381c88d871916d62e90edb60f109ff664
Meghana1301 <168684893+Meghana1301@users.noreply.github.com>
2024-12-04 22:10:02 -0500

    Update handler.js

--- Meghana_travel_backend/functions/handler.js
+++ Meghana_travel_backend/functions/handler.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 exports.handler = async (event) => {
-   const publicDir = path.join(__dirname, '..', '..', 'public', 'travel_node', 'public');
+   const publicDir = path.join(__dirname, '..', 'Meghana_travel_backend', 'public');
 
     if (event.path === '/') {
         // Serve index.html
